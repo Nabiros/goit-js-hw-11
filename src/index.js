b9297225e2507654b3dea67395516754a1697773
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-07-10 12:53:17 +0300

    added base

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './sass/main.scss';
+import switchColors from './js/switch-color';
---
+++ src/js/switch-color.js
@@ -0,0 +1,6 @@
+import refs from "../js/refs";
+import colors from '../js/colors';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
